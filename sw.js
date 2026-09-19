c55886ac2672bd381ceba3328ecf2ef38f10daf8
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 12:21:36 +0100

    Atualizar cache para v46

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='horarios-familia-2026-27-v45';
-const CORE=['./?v=45','index.html?v=45','manifest.webmanifest?v=45','icon.svg'];
+const C='horarios-familia-2026-27-v46';
+const CORE=['./?v=46','index.html?v=46','manifest.webmanifest?v=46','icon.svg'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
