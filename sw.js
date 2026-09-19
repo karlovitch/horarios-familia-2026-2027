2e1b0e4b7b23ddf2c05410d12a6bcbbf6491fa59
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 14:11:40 +0100

    Atualizar cache para v57

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='horarios-familia-2026-27-v56';
-const CORE=['./?v=56','index.html?v=56','manifest.webmanifest?v=56','icon.svg'];
+const C='horarios-familia-2026-27-v57';
+const CORE=['./?v=57','index.html?v=57','manifest.webmanifest?v=57','icon.svg'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
