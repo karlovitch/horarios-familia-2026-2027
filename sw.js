9afc4c455cf93e9457f6805e1c7b81f52cf0101f
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 02:23:55 +0100

    Atualizar cache para v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v16';
+const C='horarios-familia-2026-27-v17';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
