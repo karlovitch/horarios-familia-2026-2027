ce3b837ed920bcd722571c0783e1dc77cacfdd48
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 02:49:23 +0100

    Atualizar cache para v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v19';
+const C='horarios-familia-2026-27-v20';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
