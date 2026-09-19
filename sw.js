4a41b72842616c972fda7f02a30186d9b3416011
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 15:47:49 +0100

    Atualiza cache da aplicação para v67

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='horarios-familia-2026-27-v66';
-const CORE=['./?v=66','index.html?v=66','manifest.webmanifest?v=66','icon.svg'];
+const C='horarios-familia-2026-27-v67';
+const CORE=['./?v=67','index.html?v=67','manifest.webmanifest?v=67','icon.svg'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
