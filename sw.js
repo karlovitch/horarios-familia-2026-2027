06b8c324dd8228cbe0e1d9dc834c6b9ef48e203f
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 03:32:06 +0100

    Atualizar cache para v28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v27';
+const C='horarios-familia-2026-27-v28';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
