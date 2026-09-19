0599d9005e9ec1649036d3d98ef35d5674013ace
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 03:44:36 +0100

    Atualizar cache para v33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v32';
+const C='horarios-familia-2026-27-v33';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
