7fb5df9ea214fc24bf760001da1b6925fae6b02c
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 02:32:30 +0100

    Atualizar cache para v18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v17';
+const C='horarios-familia-2026-27-v18';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
