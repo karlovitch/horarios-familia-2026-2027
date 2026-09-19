d8bfb0c8f837c52c42e3e2a35f9d4f62f0399e1e
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 03:18:59 +0100

    Atualizar cache para v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v24';
+const C='horarios-familia-2026-27-v25';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
