ab9ede703361af655f67a23fbfdc2363d8776bfb
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 02:12:06 +0100

    Atualizar cache para v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v14';
+const C='horarios-familia-2026-27-v15';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
