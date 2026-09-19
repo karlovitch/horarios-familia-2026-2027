3dabf533d5d10d842669e41042aff940056c674a
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 03:39:35 +0100

    Atualizar cache para v31

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v30';
+const C='horarios-familia-2026-27-v31';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
