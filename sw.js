040e0141936de42c9fdcaefe283311769d0a6eab
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 17:07:59 +0100

    Atualiza service worker para v80

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD=79;
+const BUILD=80;
 const C='horarios-familia-2026-27-v'+BUILD;
 const CORE=['./?v='+BUILD,'index.html?v='+BUILD,'manifest.webmanifest?v='+BUILD,'icon.svg?v='+BUILD];
 const NETWORK_FIRST_PATHS=new Set([
