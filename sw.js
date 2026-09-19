c469eb7cd4ab5d881bb01b872b3012d22d71002a
karlovitch <61410707+karlovitch@users.noreply.github.com>
2026-09-19 03:26:35 +0100

    Atualizar cache para v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='horarios-familia-2026-27-v26';
+const C='horarios-familia-2026-27-v27';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
